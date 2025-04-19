8b7c8efeba39b34f3fe84a6bd5c8e28a2679b358
mahii29 <mahish.kadam@gmail.com>
2025-04-19 20:46:59 +0530

    Refactor layer.py to use a weights matrix and biases list for dot product calculation

--- layer.py
+++ layer.py
@@ -1,14 +1,33 @@
+import numpy as np
+
 inputs = [1, 2, 3, 2.5]
-weights1 = [0.2, 0.8, -0.5, 1.0]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
 
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
 
-output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
-          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
-          inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3]
-print(output)
+biases = [2,3,0.5]
+'''
+# Explanation of the dot product with an example:
+# The dot product between the weights matrix and the inputs vector is calculated as follows:
+# For each neuron (row in weights), multiply each weight by the corresponding input and sum them up.
+# Example for the first neuron:
+# (0.2 * 1) + (0.8 * 2) + (-0.5 * 3) + (1.0 * 2.5) = 0.2 + 1.6 - 1.5 + 2.5 = 2.8
+# Add the corresponding bias to this sum: 2.8 + 2 = 4.8
+# This process is repeated for all neurons.
 
+# The weights matrix:
+# [[ 0.2   0.8  -0.5   1.0  ],
+#  [ 0.5  -0.91  0.26 -0.5  ],
+#  [-0.26 -0.27  0.17  0.87 ]]
+
+# The biases vector:
+# [2, 3, 0.5]
+
+# The result of np.dot(weights, inputs) + biases will be:
+# [4.8, 1.21, 2.385]
+'''
+
+output = np.dot(weights, inputs)+biases
+
+print(f"layer output:- {output}")
